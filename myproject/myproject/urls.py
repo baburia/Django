66ff98ee4a3fccc339666c69fc4e094b7a7f2550
Jafeth Leyva <jafeth.leyva@hotmail.com>
2021-07-27 19:45:20 -0600

    QuerySets

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -43,8 +43,10 @@ urlpatterns = [
     path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
     name='password_change'),
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
-    name='password_change_done'),    
+    name='password_change_done'),   
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name= 'new_topic'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'), 
     path('admin/', admin.site.urls),
 ]
